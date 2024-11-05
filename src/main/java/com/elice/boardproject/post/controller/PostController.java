bba7faf404a3d64408a3ddb133a1f29f9a2bd2aa
RM <bybark4785@gmail.com>
2024-11-05 22:02:08 +0900

    Rename: "Add Post directory"

--- src/main/java/com/elice/boardproject/post/controller/PostController.java
+++ src/main/java/com/elice/boardproject/post/controller/PostController.java
@@ -6,18 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/post")
 @Controller
 public class PostController {
     @Autowired
     private PostService postService;
 
     @GetMapping("/list")
-    public String getLists(Model model) {
+    public String Lists(Model model) {
         List<Post> posts = postService.findAll();
         model.addAttribute("posts", posts);
-        return "list";
+        return "post/list";
     }
 }

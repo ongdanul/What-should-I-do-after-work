a1bf84585fff4983a3fc684eb776d3b27e8eca89
최승훈 <gogo5896544@naver.com>
2024-11-11 16:24:01 +0900

    Fix admin.xml 잘못된 경로 수정

--- src/main/java/com/elice/boardproject/admin/service/AdminService.java
+++ src/main/java/com/elice/boardproject/admin/service/AdminService.java
@@ -3,6 +3,8 @@ package com.elice.boardproject.admin.service;
 import com.elice.boardproject.admin.entity.Admin;
 import com.elice.boardproject.admin.mapper.AdminMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+    @Autowired
     private final AdminMapper adminMapper;
 
     public List<Admin> getAllProfiles() {
@@ -21,11 +24,11 @@ public class AdminService {
     public void deleteProfileByUserId(String userId) { //어드민 회원삭제
         adminMapper.deleteProfileByUserId(userId);
     }
-
+}
     //로그인 잠금 토글 미완성
     /*@Transactional
     public void toggleLoginLock(String userId, boolean currentLockState) {
         profileMapper.toggleLoginLock(userId, !currentLockState);
     }*/
-}
+
 
